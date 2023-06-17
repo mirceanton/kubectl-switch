585fe7fc3f3ca2df6db5304cbe7a8719de9d5842
Mircea-Pavel ANTON <contact@mirceanton.com>
2023-06-17 13:09:05 +0000

    Update script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cd /tmp
-wget https://raw.githubusercontent.com/mirceanton/kswitcher/v1.0.0/kswitcher.py
-wget https://raw.githubusercontent.com/mirceanton/kswitcher/v1.0.0/requirements.txt
+wget https://raw.githubusercontent.com/mirceanton/kswitcher/v1.0.0/src/kswitcher.py
+wget https://raw.githubusercontent.com/mirceanton/kswitcher/v1.0.0/src/requirements.txt
 pip install -r requirements.txt
 chmod +x kswitcher.py
 sudo mv kswitcher.py /usr/local/bin/kswitcher
