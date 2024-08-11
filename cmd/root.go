3972825e8bd92e3ccfa91b97472a41381f98966e
Mircea-Pavel ANTON <contact@mirceanton.com>
2024-08-11 12:25:40 +0000

    feat: Set up basic CLI structure

---
+++ cmd/root.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var configDir string // Global flag for the kubeconfig directory
+
+var rootCmd = &cobra.Command{
+	Use:   "kube-switcher",
+	Short: "A tool to switch Kubernetes contexts",
+	Long:  `kube-switcher is a CLI tool to switch Kubernetes contexts from multiple kubeconfig files.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Add any global flags here
+	rootCmd.PersistentFlags().StringVarP(&configDir, "kubeconfig-dir", "", "", "Directory containing kubeconfig files")
+}
